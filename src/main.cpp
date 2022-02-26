7bc2a26bcca282842e6094e0cee3d56e6e58d732
San Jacobs <R530er@gmail.com>
2022-02-26 16:31:46 +0100

    Added license and plan

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,24 @@
+/*
+satscalc - software to help with billing for Norwegian film workers
+Copyright (C) 2022 Sander Skjegstad
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see https://www.gnu.org/licenses/
+*/
+
+/* TODO: Make a system that effeciently stores a range of time, and lets you split it up neatly */
+/* TODO: Make the system that determines the price of each of those slices of time */
+
 #include <iostream>
 #include <boost/date_time/time_duration.hpp>
 
