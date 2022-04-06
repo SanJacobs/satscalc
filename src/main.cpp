57c57abcda5de8f2d2f715755db7530e318a9e62
SanJacobs <san@sparkburst.net>
2022-04-06 22:48:33 +0200

    Some progress

--- src/main.cpp
+++ src/main.cpp
@@ -16,16 +16,30 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see https://www.gnu.org/licenses/
 */
 
-/* TODO: Make a system that effeciently stores a range of time, and lets you split it up neatly */
-/* TODO: Make the system that determines the price of each of those slices of time */
+// TODO: Make a system that effeciently stores a range of time, and lets you split it up neatly
+// The slicing function could use a pointer to output the posterior half of the time range into
+// The slicing process should figure out how many slices will need to be made before doing the slices, so a correctly sized array can be allocated on the stack instead of using a vec on the heap
+
+// TODO: Make the system that determines the price of each of those slices of time
+
+// TODO: Test the boost time date system, see if it accounts for leap years and DST.
 
 #include <iostream>
-#include <boost/date_time/time_duration.hpp>
+#include "time.h"
+//#include <boost/date_time/time_duration.hpp>
+//#include <boost/date_time/posix_time/posix_time.hpp>
 
 int main()
 {
-	tm test_moment_one{0, 30, 14, 27, 11, 2010};
-	std::cout << "Testing 123" << std::endl;
+	//using boost::posix_time::to_simple_string;
+	//tm test_moment_one{0, 30, 14, 27, 11, 2010};
+	moment calltime{30, 8, 27, 11, 2010};
+	moment wraptime{30, 16, 27, 11, 2010};
+	timeblock workday{calltime, wraptime};
+	
+	
+	std::cout << "Testing 123\n";
+	std::cout << timeprint(workday.start) << std::endl;
 	return 0;
 }
 
